bc4ba3b69dc53e275a64eb8f441b307e6d8cdf6c
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2020-08-01 20:06:49 +0300

    ex 2

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,5 +1,17 @@
 import math
 
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+def isint(value):
+    try:
+        return int(value) == float(value)
+    except ValueError:
+        return False
 while True:
     try:
         k = int(input("==========================\n"
@@ -43,23 +55,84 @@ while k != 0:
                 print("Ошибка, введен не верный номер задания. Попробуйте снова.")
                 continue
             break
-        while not(k2 == 0):
+        a = [1,-2.78,3, "dscdc", 5, 3, 5, 1, "dscdc"]
+        while k2 != 0:
             if k2 == 1:
-                print("Задание №2.1")
+                print(a)
             elif k2 == 2:
-                print("Задание №2.2")
+                x = (input("Ввведите значение: "))
+                a.append(x)
             elif k2 == 3:
-                print("Задание №2.3")
+                print("1. Удалить по номеру в списке.\n"
+                      "2. Удалить по значению.")
+                while True:
+                    try:
+                        k4 = int(input("Выберете способ удаления: "))
+                    except:
+                        print("Ошибка, введен не верный способ удаления. Попробуйте снова.")
+                        continue
+                    break
+                if k4 == 1:
+                    while True:
+                        try:
+                            x = int(input("Ввведите номер: "))
+                        except:
+                            print("Ошибка, введен не верный способ удаления. Попробуйте снова.")
+                            continue
+                        break
+                    try:
+                        a.pop(x)
+                    except:
+                        print("Ошибка, элемента с данным номером не существует.")
+                elif k4 == 2:
+                    while True:
+                        try:
+                            x = int(input("Ввведите значение: "))
+                        except:
+                            print("Ошибка, введен не верный способ удаления. Попробуйте снова.")
+                            continue
+                        break
+                    try:
+                        a.remove(x)
+                    except:
+                        print("Ошибка, элемента с данным значением не существует.")
+                else: print("Ошибка, введен не верный способ удаления. Попробуйте снова.")
             elif k2 == 4:
-                print("Задание №2.4")
+                b = []
+                for i in range(len(a)):
+                   if isfloat(a[i]):
+                       if a[i] > 0:
+                             b.append(a[i])
+                print("Кортеж: ")
+                print(tuple(b))
             elif k2 == 5:
-                print("Задание №2.5")
+                p = int(1)
+                for i in range(len(a)):
+                    if isint(a[i]):
+                       p = int(p) * int(a[i])
+                print("Произведение: " + str(p))
             elif k2 == 6:
-                print("Задание №2.6")
+                s = str("")
+                for i in range(len(a)):
+                    s = str(s) + " " + str(a[i])
+                s2 = str(input("Введите строку для поиска: "))
+                print("Введенное слово встретилось: " +str(s.count(s2)))
             elif k2 == 7:
-                print("Задание №2.7")
+                b = {i for i in range(10)}
+                print(set(a))
+                print(b)
+                # множество, состоящее только из уникальных элементов исходных множеств
+                print("Симметричная разница: ")
+                print(set(a)^b)
             elif k2 == 8:
-                print("Задание №2.8")
+                d = { }
+                for i in range(len(a)):
+                    d[i] = a[i]
+                print("Словарь: ")
+                print(d)
+                for i in range(len(a)):
+                   if i%2 != 0:
+                       print("Элемент " + str(i) + ": " +str(d[i]))
             else:
                 print("Ошибка, введен не верный номер задания. Попробуйте снова.")
             while True:
